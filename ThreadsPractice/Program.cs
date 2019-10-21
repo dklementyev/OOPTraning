f0bb0a33870c8a3cf02ece73fe11831a6a0f3fff
DKlementyev <dmitriklementev@gmail.com>
2019-10-21 21:39:36 +0300

    add project for traning Threads and Tasks

---
+++ ThreadsPractice/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadsPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var thread1 = new Thread(ThreadFunction1);
+            var thread2 = new Thread(ThreadFunction2);
+            thread1.Start();
+            thread2.Start();
+
+            for (var i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Main Thread talks: {i}");
+                Thread.Sleep(100);
+            }
+
+            Console.Read();
+        }
+
+
+
+        public static void ThreadFunction1()
+        {
+            for (var i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Thread1 talks: {i}");
+                Thread.Sleep(100);
+            }
+        }
+        public static void ThreadFunction2()
+        {
+            for (var i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Thread2 talks: {i}");
+                Thread.Sleep(100);
+            }
+        }
+
+
+    }
+}
